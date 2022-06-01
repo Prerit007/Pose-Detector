52579d05f35742fa8302e038f55b47d61c5d87f3
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-06-01 21:42:55 +0530

    Main file

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+import time
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture("PoseVideos/6.mp4")    # Change the Input to 0 for using Webcam
+cap.set(3, 1280)
+cap.set(4, 720)
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 4, (210, 220, 80), thickness=2)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_COMPLEX, 3, (210, 220, 80), 3)
+    cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
